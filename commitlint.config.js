8380bd3bc35e9c57e3c643ca461da231eb08ee80
brianframework <139675965+brianframework@users.noreply.github.com>
2025-01-13 17:59:39 -0800

    refactor: migrate SCSS imports to use syntax

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-};
+  rules: {
+    'type-empty': [0],
+    'subject-empty': [0],
+    'type-enum': [0],
+    'subject-case': [0],
+    'header-max-length': [0],
+    'body-leading-blank': [0],
+    'footer-leading-blank': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'scope-enum': [0],
+    'subject-full-stop': [0]
+  }
+}
