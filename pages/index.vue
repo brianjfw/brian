953d3c09a16d7145d9310523ae8777e0440fddc2
brianframework <139675965+brianframework@users.noreply.github.com>
2025-01-13 18:15:30 -0800

    refactor: remove unused SCSS imports from index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -94,11 +94,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@use '../assets/scss/constants/animation';
-@use '../assets/scss/constants/color' as *;
-@use '../assets/scss/constants/font' as *;
-@use '../assets/scss/functions/mixins' as *;
-
 .index {
   overflow: hidden;
 }
