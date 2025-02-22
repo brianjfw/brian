ce503dfd062aed5731b72dd5d48b588bd7b064b2
brianframework <139675965+brianframework@users.noreply.github.com>
2025-02-21 19:42:11 -0800

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,20 +19,24 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.jpg', sizes: '16x16', type: 'image/jpeg' },
+      { url: '/favicon-32x32.jpg', sizes: '32x32', type: 'image/jpeg' },
     ],
     apple: [
-      { url: '/apple-touch-icon.png' },
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
     other: [
       {
-        rel: 'android-chrome-192x192',
+        rel: 'mask-icon',
         url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
       },
       {
-        rel: 'android-chrome-512x512',
+        rel: 'mask-icon',
         url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
       },
     ],
   },
